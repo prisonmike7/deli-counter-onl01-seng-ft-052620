2cf53c910cf7bb3cf1c73bda5ee7bd3e38a51425
Michael Harris <mwharris7@gmail.com>
2020-05-28 08:08:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli = []
+
+def line(line_queue)
+  if line_queue.size == 0
+    puts "The line is currently empty."
+    return
+  puts
+end
+
+def take_a_number(line_queue, name)
+  line_queue << name
+  puts "Welcome, #{name}. You are number #{line_queue.size} in line.""
+end
+
+def now_serving(line_queue)
+  if line_queue.size == 0
+    puts "There is nobody waiting to be served!"
+    return
+  next_name = line_queue.shift
+  puts
+end
